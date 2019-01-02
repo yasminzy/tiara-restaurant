5e01d32938b8c4ff28bc667133268f3df537a178
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-02 21:26:35 +0800

    Use lazy routing

--- src/router.js
+++ src/router.js
@@ -1,20 +1,21 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Contact from "./views/contact.vue";
-import Home from "./views/home.vue";
-import Reservations from "./views/reservations.vue";
-import Sent from "./views/sent.vue";
+import Home from "./views/home";
 
 Vue.use(Router);
 
 export default new Router({
+  base: process.env.BASE_URL,
   mode: "history",
   routes: [
     { path: "/", component: Home },
-    { path: "/contact", component: Contact },
-    { path: "/reservations", component: Reservations },
-    { path: "/sent", component: Sent }
+    { path: "/contact", component: () => import("./views/contact.vue") },
+    {
+      path: "/reservations",
+      component: () => import("./views/reservations.vue")
+    },
+    { path: "/sent", component: () => import("./views/sent.vue") }
   ],
   scrollBehavior() {
     return { x: 0, y: 0 };
